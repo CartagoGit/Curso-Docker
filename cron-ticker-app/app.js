95397bca1686dfb8e81e13f333d0e06a82e6135e
Cartago <cartago.relaxingcup@gmail.com>
2023-06-12 17:37:13 +0200

    Creamos un crono que se ejecuta cada 5 segundos

--- cron-ticker-app/app.js
+++ cron-ticker-app/app.js
@@ -1,1 +1,9 @@
-console.log('Hello World')
+const cron = require("node-cron");
+let times = 0;
+
+cron.schedule("*/5 * * * * *", () => {
+	times++;
+	console.log("Tick cada 5 segundos", `Veces que se ha repetido: ${times}`);
+});
+
+console.log("Inicio");
