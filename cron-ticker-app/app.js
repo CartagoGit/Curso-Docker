eaa4cb0730675cd953da11eb1835e156727508d5
Cartago <cartago.relaxingcup@gmail.com>
2023-06-12 17:56:16 +0200

    Creamos nuestro primer dockerfile para crear la primera imagen

--- cron-ticker-app/app.js
+++ cron-ticker-app/app.js
@@ -3,7 +3,7 @@ let times = 0;
 
 cron.schedule("*/5 * * * * *", () => {
 	times++;
-	console.log("Tick cada 5 segundos", `Veces que se ha repetido: ${times}`);
+	console.log("Tick cada 5 segundos.", `Veces que se ha repetido: ${times}`);
 });
 
 console.log("Inicio");
